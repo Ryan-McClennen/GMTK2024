33005654e5afcbe70a7b1dfab3a90cc996cb67fe
Masmooo <167024358+Masmooo@users.noreply.github.com>
2024-08-16 11:04:09 -0400

    Add files via upload
    2D side scroll movement + jump

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 8f;
+    private float horizontal;
+    [SerializeField]
+    private float jumpingPow = 16f;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    // object used for checking if player is on ground
+    [SerializeField]
+    private Transform groundCheck;
+
+    // layer type to identify which objects are ground
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        // jump
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPow);
+        }
+
+        // stay longer in air after jump
+        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);  
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        // spawns circle to see if it overlaps with ground objects under player.
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+}
