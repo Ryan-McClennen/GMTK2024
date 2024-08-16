76a02b3e3f58964e1e7d5ae54585878cf0b326a0
Masmooo <167024358+Masmooo@users.noreply.github.com>
2024-08-16 11:05:42 -0400

    Added side scroll move + jump script

--- Assets/Scripts/SideScrollMove.cs
+++ Assets/Scripts/SideScrollMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class SideScrollMove : MonoBehaviour
 {
     [SerializeField]
     private float speed = 8f;
