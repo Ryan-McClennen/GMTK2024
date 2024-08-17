75b2ee4913eadd3dfb0abf025507b71683729be9
Ryan-McClennen <rmcclennen3@gmail.com>
2024-08-16 20:43:03 -0400

    Added slerp to vertical movement

--- Assets/Scripts/BalloonMovement.cs
+++ Assets/Scripts/BalloonMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -11,8 +12,8 @@ public class BalloonMovement : MonoBehaviour
     private float horizontal;
     private float vertical;
 
-    public int balloonNumber = -1;
-    public int floatinessScale = 0;
+    public float balloonNumber = 0;
+    public int[] floatinessScale = {0, 25, 55, 70, 75, 85, 100, 110};
     
 
     [SerializeField]
@@ -26,88 +27,42 @@ public class BalloonMovement : MonoBehaviour
     [SerializeField]
     private LayerMask groundLayer;
 
-    private void Update()
-    {
-        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && balloonNumber < 5)
-        {
-            Debug.Log("Up: Balloon Level = " + (balloonNumber + 1));
-            Inflate();
-        }
-
-        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && balloonNumber > -1)
-        {
-            Debug.Log("Down: Balloon Level = " + (balloonNumber - 1));
-            Deflate();
-        }
-    }
-
     private void FixedUpdate()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        balloonNumber += Input.GetAxisRaw("Vertical") * 0.05f;
+        balloonNumber = Mathf.Clamp(balloonNumber, 0, floatinessScale.Length - 1);
+
+        if(balloonNumber <= 0.25f) rb.gravityScale = 3.2f;
+        else rb.gravityScale = 1.2f;
+
         if (!IsGrounded())
         {
-            rb.AddForce((horizontal * speed * Vector2.right) + (floatinessScale * 0.18f * Vector2.up));
+            rb.AddForce((horizontal * speed * Vector2.right) + (GetFloatiness() * 0.18f));
         }
         else
         {
-            if(balloonNumber < 1)
-            {
-                rb.gravityScale = 3.2f;
-            }
             rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-            rb.AddForce(floatinessScale * 0.2f * Vector2.up);
+            rb.AddForce(GetFloatiness() * 0.2f);
         }
     }
-    // helloo
+
     private bool IsGrounded()
     {
         // spawns circle to see if it overlaps with ground objects under player.
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
-    private void Inflate()
+    private Vector2 GetFloatiness()
     {
-        balloonNumber++;
-        ChangeFloatiness();
-    }
-
-    private void Deflate()
-    {
-        balloonNumber--;
-        ChangeFloatiness();
-    }
-
-    private void ChangeFloatiness()
-    {
-        if (balloonNumber == -1)
-        {
-            floatinessScale = 0;
-        }
-        else if (balloonNumber == 0)
-        {
-            floatinessScale = 55;
-        }
-        else if(balloonNumber == 1)
-        {
-            floatinessScale = 70;
-            rb.gravityScale = 1.2f;
-        }
-        else if (balloonNumber == 2)
-        {
-            floatinessScale = 75;
-        }
-        else if (balloonNumber == 3)
-        {
-            floatinessScale = 85;
-        }
-        else if (balloonNumber == 4)
-        {
-            floatinessScale = 100;
-        }
-        else if (balloonNumber == 5)
-        {
-            floatinessScale = 110;
-        }
+        int lower = (int) balloonNumber;
+        int upper = Mathf.Min((int) balloonNumber + 1, floatinessScale.Length - 1);
+        float remainer = balloonNumber % 1;
+        Vector3 slerped = Vector3.Slerp(new Vector3(0, floatinessScale[lower], 0),
+                                        new Vector3(0, floatinessScale[upper], 0),
+                                        remainer);
+        return new Vector2(0, slerped.y);
     }
 }
