931ac3e3f6a990dad9da9a4dd8263d5ad8287535
Ryan-McClennen <rmcclennen3@gmail.com>
2024-08-16 23:44:02 -0400

    Finalized player movement and function

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -5,4 +5,43 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
+    [Header ("Child Data")]
+    public GameObject child;
+    public BalloonMovement balloonMove;
+
+    [Header ("Robot Data")]
+    public GameObject robot;
+    public RobotMovement robotMove;
+
+    private bool isChild;
+
+    public void Start()
+    {
+        isChild = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (balloonMove.IsGrounded() && isChild)
+            {
+                balloonMove.UnsetAsPlayer();
+                robotMove.SetAsPlayer();
+                isChild = false;
+            }
+
+            else if (Vector2.Distance(robot.transform.position, child.transform.position) < 0.5 && !isChild)
+            {
+                balloonMove.SetAsPlayer();
+                robotMove.UnsetAsPlayer();
+                isChild = true;
+            }
+        }
+
+        if (isChild)
+        {
+            robot.transform.position = child.transform.position + new Vector3(0.5f, 0.5f, 1f);
+        }
+    }
 }
