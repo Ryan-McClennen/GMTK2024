c26d169a93fa19c835bee2b27aa988c7b0a6967d
Masmooo <robloxisthebestgame818@gmail.com>
2024-08-17 12:39:39 -0400

    Dual tile system with brass + dirt + grass

--- Assets/Scripts/DualGridTilemap.cs
+++ Assets/Scripts/DualGridTilemap.cs
@@ -15,15 +15,18 @@ public class DualGridTilemap : MonoBehaviour {
 
     protected static Dictionary<Tuple<TileType, TileType, TileType, TileType>, Tile> neighbourTupleToTile;
     protected static Dictionary<Tuple<TileType, TileType, TileType, TileType>, Tile> neighbourTupleToTileGrass;
+    protected static Dictionary<Tuple<TileType, TileType, TileType, TileType>, Tile> neighbourTupleToTileBrass;
 
     // Provide references to each tilemap in the inspector
     public Tilemap placeholderTilemap;
     public Tilemap displayTilemap;
     public Tilemap displayTilemapTwo;
+    public Tilemap displayTilemapThree;
 
     // Provide the dirt and grass placeholder tiles in the inspector
     public Tile grassPlaceholderTile;
     public Tile dirtPlaceholderTile;
+    public Tile brassPlaceholderTile;
 
     // Provide the 16 tiles in the inspector
     public Tile[] tiles;
@@ -71,6 +74,27 @@ public class DualGridTilemap : MonoBehaviour {
             {new (Dirt, Dirt, Dirt, Dirt), tiles[28]},
 
         };
+
+        neighbourTupleToTileBrass = new() {
+            {new (None, None, None, None), tiles[38]},
+            {new (Brass, Brass, Brass, None), tiles[45]}, // OUTER_BOTTOM_RIGHT
+            {new (Brass, Brass, None, Brass), tiles[32]}, // OUTER_BOTTOM_LEFT
+            {new (Brass, None, Brass, Brass), tiles[40]}, // OUTER_TOP_RIGHT
+            {new (None, Brass, Brass, Brass), tiles[47]}, // OUTER_TOP_LEFT
+            {new (Brass, None, Brass, None), tiles[33]}, // EDGE_RIGHT
+            {new (None, Brass, None, Brass), tiles[43]}, // EDGE_LEFT
+            {new (Brass, Brass, None, None), tiles[35]}, // EDGE_BOTTOM
+            {new (None, None, Brass, Brass), tiles[41]}, // EDGE_TOP
+            {new (Brass, None, None, None), tiles[37]}, // INNER_BOTTOM_RIGHT
+            {new (None, Brass, None, None), tiles[34]}, // INNER_BOTTOM_LEFT
+            {new (None, None, Brass, None), tiles[42]}, // INNER_TOP_RIGHT
+            {new (None, None, None, Brass), tiles[39]}, // INNER_TOP_LEFT
+            {new (Brass, None, None, Brass), tiles[46]}, // DUAL_UP_RIGHT
+            {new (None, Brass, Brass, None), tiles[36]}, // DUAL_DOWN_RIGHT
+            {new (Brass, Brass, Brass, Brass), tiles[44]},
+
+        };
+
         RefreshDisplayTilemap();
     }
 
@@ -79,21 +103,35 @@ public class DualGridTilemap : MonoBehaviour {
         setDisplayTile(coords);
     }
 
-    private TileType getPlaceholderTileTypeAt(Vector3Int coords) {
-        if (placeholderTilemap.GetTile(coords) == grassPlaceholderTile)
-            return Grass;
-        else if (placeholderTilemap.GetTile(coords) == dirtPlaceholderTile)
-            return Dirt;
+    private TileType getPlaceholderTileTypeAt(Vector3Int coords, String tileSetName) {
+        if (tileSetName != "Brass")
+        {
+            if (placeholderTilemap.GetTile(coords) == grassPlaceholderTile)
+                return Grass;
+            else if (placeholderTilemap.GetTile(coords) == dirtPlaceholderTile)
+                return Dirt;
+            else
+                return None;
+        }
         else
-            return None;
+        {
+            if(placeholderTilemap.GetTile(coords) == brassPlaceholderTile)
+            {
+                return Brass;
+            }
+            else
+            {
+                return None;
+            }
+        }
     }
 
-    protected Tile calculateDisplayTile(Vector3Int coords, Dictionary<Tuple<TileType, TileType, TileType, TileType>, Tile> tileSet) {
+    protected Tile calculateDisplayTile(Vector3Int coords, Dictionary<Tuple<TileType, TileType, TileType, TileType>, Tile> tileSet, String tileSetName) {
         // 4 neighbours
-        TileType topRight = getPlaceholderTileTypeAt(coords - NEIGHBOURS[0]);
-        TileType topLeft = getPlaceholderTileTypeAt(coords - NEIGHBOURS[1]);
-        TileType botRight = getPlaceholderTileTypeAt(coords - NEIGHBOURS[2]);
-        TileType botLeft = getPlaceholderTileTypeAt(coords - NEIGHBOURS[3]);
+        TileType topRight = getPlaceholderTileTypeAt(coords - NEIGHBOURS[0], tileSetName);
+        TileType topLeft = getPlaceholderTileTypeAt(coords - NEIGHBOURS[1], tileSetName);
+        TileType botRight = getPlaceholderTileTypeAt(coords - NEIGHBOURS[2], tileSetName);
+        TileType botLeft = getPlaceholderTileTypeAt(coords - NEIGHBOURS[3], tileSetName);
 
         Tuple<TileType, TileType, TileType, TileType> neighbourTuple = new(topLeft, topRight, botLeft, botRight);
 
@@ -103,8 +141,9 @@ public class DualGridTilemap : MonoBehaviour {
     protected void setDisplayTile(Vector3Int pos) {
         for (int i = 0; i < NEIGHBOURS.Length; i++) {
             Vector3Int newPos = pos + NEIGHBOURS[i];
-            displayTilemap.SetTile(newPos, calculateDisplayTile(newPos, neighbourTupleToTile));
-            displayTilemapTwo.SetTile(newPos, calculateDisplayTile(newPos, neighbourTupleToTileGrass));
+            displayTilemap.SetTile(newPos, calculateDisplayTile(newPos, neighbourTupleToTile, "Dirt"));
+            displayTilemapTwo.SetTile(newPos, calculateDisplayTile(newPos, neighbourTupleToTileGrass, "Grass"));
+            displayTilemapThree.SetTile(newPos, calculateDisplayTile(newPos, neighbourTupleToTileBrass, "Brass"));
         }
     }
 
@@ -121,5 +160,6 @@ public class DualGridTilemap : MonoBehaviour {
 public enum TileType {
     None,
     Grass,
-    Dirt
+    Dirt,
+    Brass
 }
