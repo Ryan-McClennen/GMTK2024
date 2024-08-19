b9fc6d95eb0a2ac5e95fc85664359c575bdd6209
Masmooo <robloxisthebestgame818@gmail.com>
2024-08-19 12:46:58 -0400

    background working?

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void Fade()
+    {
+
+    }
+}
