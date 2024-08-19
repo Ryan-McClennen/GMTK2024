d03f45afc4c6bf8bcc0e35dbe46006aeb0b12510
Ryan-McClennen <rmcclennen3@gmail.com>
2024-08-19 18:07:51 -0400

    Added light stuff

--- Assets/Scripts/Obstacles/DeflateButton.cs
+++ Assets/Scripts/Obstacles/DeflateButton.cs
@@ -21,17 +21,22 @@ public class DeflateButton : MonoBehaviour
     [SerializeField]
     Sprite[] buttons;
 
+    [SerializeField]
+    GameObject buttonLight;
+
     void Update()
     {
         if (hitbox.IsTouchingLayers(interactLayer))
         {
             box.isShrinking = true;
             render.sprite = buttons[1];
+            buttonLight.SetActive(true);
         }
         else
         {
             box.isShrinking = false;
             render.sprite = buttons[0];
+            buttonLight.SetActive(false);
         }
     }
 }
