0e6c8c5ee3ec253c080364c50a95ddacb7e2f1f2
Ryan-McClennen <rmcclennen3@gmail.com>
2024-08-20 03:15:45 -0400

    Bird changes

--- Assets/Scripts/Obstacles/BirdFlight.cs
+++ Assets/Scripts/Obstacles/BirdFlight.cs
@@ -7,8 +7,13 @@ public class BirdFlight : MonoBehaviour
 {
     [SerializeField]
     private int speed;
+
     [SerializeField]
     Vector3 destination;
+
+    [SerializeField]
+    float startDelay = 0f;
+
     [SerializeField]
     private float waitTime = 2f;
 
@@ -18,15 +23,16 @@ public class BirdFlight : MonoBehaviour
     [SerializeField]
     private LayerMask layer;
 
-    private bool ableToFly = true;
+    private bool ableToFly;
 
     private Vector3 pos1;
     private Vector3 pos2;
     private void Start()
     {
-        StartCoroutine(RandomWait(Random.Range(0f, 3f)));
         pos1 = transform.position;
         pos2 = destination;
+        ableToFly = false;
+        StartCoroutine(DelayStart());
     }
     // Update is called once per frame
     void Update()
@@ -55,8 +61,10 @@ public class BirdFlight : MonoBehaviour
         pos1 = temp;
         ableToFly = true;
     }
-    IEnumerator RandomWait(float time)
+
+    IEnumerator DelayStart()
     {
-        yield return new WaitForSeconds(time);
+        yield return new WaitForSeconds(startDelay);
+        ableToFly = true;
     }
 }
